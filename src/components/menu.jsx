6e1fe5aeaa9b4f0803eae58cfdd06502cfee5255
bianca-tsuchiya <bianca.tsuchiya@sptech.school>
2023-10-05 20:28:29 -0300

    projeto com react + componente menu

---
+++ src/components/menu.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import "./css/menu.css";
+
+
+function Menu() {
+    return (
+        <>
+            <body>
+                <header>
+                    <nav class="cabecalho-menu">
+                        <ul class="menu-navbar">
+
+                            <div class="btn-lateral-menu">
+                                <i class="bi bi-list"></i>
+                                <img class="logo-img-navbar" src="./imgs/logo-branca-footer.png" alt="logo branca da Woof Joy"></img>
+                            </div>
+                            <div class="space-menor"></div>
+                            <div class="todos-menu-item">
+                                <li class="menu-item">
+                                    <a href="#">
+                                        <span class="icon"><i class="bi bi-house-heart-fill"></i></span>
+                                        <span class="txt-link">Feed</span>
+                                    </a>
+                                </li>
+                                <li class="menu-item">
+                                    <a href="#">
+                                        <span class="icon"><i class="bi bi-chat-dots-fill"></i></span>
+                                        <span class="txt-link">Chat</span>
+                                    </a>
+                                </li>
+                                <li class="menu-item">
+                                    <a href="#">
+                                        <span class="icon"><i class="bi bi-bag-heart-fill"></i></span>
+                                        <span class="txt-link">Doação</span>
+                                    </a>
+                                </li>
+                                <li class="menu-item">
+                                    <a href="#">
+                                        <span class="icon"><i class="bi bi-clock-history"></i></span>
+                                        <span class="txt-link">Histórico</span>
+                                    </a>
+                                </li>
+                            </div>
+                            <div class="space"></div>
+                            <div class="menu-item-meu-perfil">
+                                <a href="#">
+                                    <span class="icon"><i class="bi bi-person-circle"></i></span>
+                                    <span class="txt-link">Meu Perfil</span>
+                                </a>
+                            </div>
+                            <div class="space-menor"></div>
+                            <hr></hr>
+                                <div class="space"></div>
+                                <div class="menu-item-sair">
+                                    <a href="#">
+                                        <span class="icon"><i class="bi bi-box-arrow-left"></i></span>
+                                        <span class="txt-link">Sair</span>
+                                    </a>
+                                </div>
+                                <div class="space"></div>
+                        </ul>
+                    </nav>
+                </header>
+            </body>
+        </>
+    );
+}
+export default Menu;
